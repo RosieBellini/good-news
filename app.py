0e797614ebd9d7bf44392c2482b9db9535973fcc
Daniel Welsh <danjwelsh24@gmail.com>
2017-01-20 18:21:08 +0000

    DW: + Changes to db connection

--- app.py
+++ app.py
@@ -37,13 +37,13 @@ def save_headlines():
 
     db = MySQLdb.connect(host="localhost", user=user, passwd=password, db=database)  # name of the data base
     db.set_character_set('utf8')
-    db.execute('SET NAMES utf8;')
-    db.execute('SET CHARACTER SET utf8;')
-    db.execute('SET character_set_connection=utf8;')
 
     # you must create a Cursor object. It will let
     #  you execute all the queries you need
     cur = db.cursor()
+    cur.execute('SET NAMES utf8;')
+    cur.execute('SET CHARACTER SET utf8;')
+    cur.execute('SET character_set_connection=utf8;')
 
     # Use all the SQL you like
     cur.execute("SELECT * FROM headlines")
